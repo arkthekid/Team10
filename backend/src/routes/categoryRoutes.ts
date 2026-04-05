c11f782387e411550a47bc20e9cffcfbad53aebf
Ngan Nguyen <nhnguyen@umass.edu>
2026-04-05 00:06:04 -0400

    refactor: delete category files

--- backend/src/routes/categoryRoutes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { createCategory } from "../controllers/categoryController";
-
-const router = Router();
-router.post("/", createCategory);
-
-export default router;
