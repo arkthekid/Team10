307b8ce46b7f3eff8b679e31b8ab7c745ef23ea8
Arkar Myint <arkarmoemyint100@gmail.com>
2026-04-16 19:05:04 -0400

    add Conversation and Message entitites

---
+++ backend/src/entities/Conversation.ts
@@ -0,0 +1,55 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  OneToMany,
+  JoinColumn,
+} from "typeorm";
+import { User } from "./User";
+import { Listing } from "./Listing";
+import { Message } from "./Message";
+
+@Entity()
+export class Conversation {
+  @PrimaryGeneratedColumn("uuid")
+  id!: string;
+
+  @Column("text")
+  listingId!: string;
+
+  @Column("text")
+  buyerId!: string;
+
+  @Column("text")
+  sellerId!: string;
+
+  @Column({ default: false })
+  isArchived!: boolean;
+
+  @Column({ default: false })
+  isMuted!: boolean;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+
+  @UpdateDateColumn()
+  updatedAt!: Date;
+
+  @ManyToOne(() => Listing)
+  @JoinColumn({ name: "listingId" })
+  listing!: Listing;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: "buyerId" })
+  buyer!: User;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: "sellerId" })
+  seller!: User;
+
+  @OneToMany(() => Message, (message: Message) => message.conversation)
+  messages!: Message[];
+}
