3b1baee89d61370c9219317e853c800e48f009ec
Arkar Myint <arkarmoemyint100@gmail.com>
2026-04-16 19:47:54 -0400

    feat: add Google OAuth for Release 2

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import { asyncHandler } from "../middleware/asyncHandler";
 import * as authService from "../services/authService";
+import * as googleAuthService from "../services/googleAuthService";
+import { AppError } from "../utils/AppError";
 
 export const register = asyncHandler(async (req: Request, res: Response) => {
   const result = await authService.register(req.body);
@@ -14,4 +16,11 @@ export const login = asyncHandler(async (req: Request, res: Response) => {
 
 export const getMe = asyncHandler(async (req: Request, res: Response) => {
   return res.json(req.user);
+});
+
+export const googleAuth = asyncHandler(async (req: Request, res: Response) => {
+  const { idToken } = req.body;
+  if (!idToken) throw new AppError("Google ID token is required", 400);
+  const result = await googleAuthService.googleLogin(idToken);
+  return res.json(result);
 });
