f870d692eea3341ac3d325653af3c2e399fbb7c6
Arkar Myint <arkarmoemyint100@gmail.com>
2026-04-16 21:16:07 -0400

    fix: data-source.ts

--- backend/src/config/data-source.ts
+++ backend/src/config/data-source.ts
@@ -3,22 +3,20 @@ import { DataSource } from "typeorm";
 import { Listing } from "../entities/Listing";
 import { User } from "../entities/User";
 import { Favorite } from "../entities/Favorite";
+import { Conversation } from "../entities/Conversation";
+import { Message } from "../entities/Message";
 
 console.log("DATABASE_URL:", process.env.DATABASE_URL);
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  
-  // host: process.env.DB_HOST ?? "localhost",
-  // port: Number(process.env.DB_PORT) || 5432,
-  // username: process.env.DB_USER ?? "postgres",
-  // password: process.env.DB_PASSWORD ?? "",
-  // database: process.env.DB_NAME ?? "mydb",
-  url: process.env.DATABASE_URL!,
-
-  ssl: { rejectUnauthorized: false }, // 👈 add this for cloud DBs
-
-  synchronize: true, // ✅ CHANGED: disable in production (use migrations instead)
+  host: process.env.DB_HOST ?? "localhost",
+  port: Number(process.env.DB_PORT) || 5432,
+  username: process.env.DB_USER ?? "postgres",
+  password: process.env.DB_PASSWORD ?? "",
+  database: process.env.DB_NAME ?? "mydb",
+  ssl: { rejectUnauthorized: false },
+  synchronize: true,
   logging: false,
-  entities: [Listing, User, Favorite],
+  entities: [Listing, User, Favorite, Conversation, Message],
 });
