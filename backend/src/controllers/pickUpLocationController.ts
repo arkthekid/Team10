2726a30a2c6f9ae6c4995dc8bb0ae9e4ac252e06
Arkar Myint <arkarmoemyint100@gmail.com>
2026-04-25 21:41:45 -0400

    feat: add createPickUpLocation controller handler

--- backend/src/controllers/pickUpLocationController.ts
+++ backend/src/controllers/pickUpLocationController.ts
@@ -1,3 +1,22 @@
 import { Request, Response, NextFunction } from "express";
 import { AppError } from "../utils/AppError";
 import * as pickUpLocationService from "../services/pickUpLocationService";
+
+export const createPickUpLocation = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { name } = req.body;
+
+    if (!name) {
+      return next(new AppError("Name is required", 400));
+    }
+
+    const location = await pickUpLocationService.createPickUpLocation(name);
+    res.status(201).json(location);
+  } catch (error) {
+    next(error);
+  }
+};
